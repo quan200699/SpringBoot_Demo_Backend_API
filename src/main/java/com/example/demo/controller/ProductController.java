ac6f64543a6c8d20a16cc103bfd59fb1f831c279
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-12-24 15:45:52 +0700

    done

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,56 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Product;
+import com.example.demo.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private IProductService productService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Product>> getAll() {
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createNewProduct(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> productOptional = productService.findById(id);
+        return productOptional.map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProductInformation(@PathVariable Long id, @RequestBody Product product) {
+        Optional<Product> productOptional = productService.findById(id);
+        return productOptional.map(product1 -> {
+            product.setId(product1.getId());
+            if (product.getName().equalsIgnoreCase("")) {
+                product.setName(product1.getName());
+            }
+            return new ResponseEntity<>(productService.save(product), HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable Long id) {
+        Optional<Product> productOptional = productService.findById(id);
+        return productOptional.map(product -> {
+            productService.remove(id);
+            return new ResponseEntity<Product>(HttpStatus.NO_CONTENT);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private double price;
+}
